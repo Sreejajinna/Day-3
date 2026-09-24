a31150936c61fec3d5e47bc58e488ce6f3890799
Sreejajinna <sreejajinnaa@gmail.com>
2026-09-24 11:00:06 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,56 @@
+
+'''
+----- Assigment operator -----
+
+--> Assiging the values of the variables
+eg: a=10
+
+here = represents the assigment operator
+
+'''
+
+a=int(input("Enter the number: "))
+print(f"Intially the value of a is {a}")
+
+a+=20
+#take a = 10
+'''
+a+=20
+a=a+20
+a=10+20
+a=30
+
+'''
+print(f"After the value of a is: {a}")
+
+b=30
+b-=25
+'''
+b-=25
+b=b-25
+b=30-25
+b=5
+
+'''
+print(b)
+
+c=50
+c+=25
+c-=25
+c*=25
+c/=25
+c//=25
+c%=25
+print(c)
+
+'''
+= --> is used to assign the value to the variable
+== ---> is used to compare the relation b/w two variables
+
+'''
+a=10
+b=20
+
+a==b
+print(a)
+print(b)
